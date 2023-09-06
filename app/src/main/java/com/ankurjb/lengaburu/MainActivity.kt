00b07e884ed16673bc176e69719784a103b584b4
Ankur <ankursinghjb@gmail.com>
2023-09-06 16:38:05 +0530

    Hilt setup

--- app/src/main/java/com/ankurjb/lengaburu/MainActivity.kt
+++ app/src/main/java/com/ankurjb/lengaburu/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ankurjb.lengaburu
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -11,8 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ankurjb.lengaburu.ui.theme.LengaburuTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
---
+++ app/src/main/java/com/ankurjb/lengaburu/MainApplication.kt
@@ -0,0 +1,7 @@
+package com.ankurjb.lengaburu
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()
